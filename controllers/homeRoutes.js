af4ffcdfb229baf6f443174a16191c468476b094
Anuka <anukazan@gmail.com>
2022-07-05 19:02:35 -0700

    add handlebar.js files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const path = require('path');
 const { User } = require("../models/user");
 
-router.get('/', (req, res) =>
+/* router.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
-);
+); */
+
+
 
 
 module.exports = router;
